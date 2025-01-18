7858e7fb3e278965135f7d4583b199af981d2a50
김경민 <kyungmin@gimgyeongmins-MacBook-Air.local>
2025-01-18 14:13:45 +0900

    프로젝트 환경 설정

---
+++ src/main/java/io/github/socialping/SocialPingApplication.java
@@ -0,0 +1,13 @@
+package io.github.socialping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialPingApplication.class, args);
+    }
+
+}
